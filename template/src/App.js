0b05afff0ae1a2b6c81dca0a46ae3698df151195
Pius Lawal <pius.lawal@ansgroup.co.uk>
2021-03-03 21:13:35 +0000

    add long line of text

--- template/src/App.js
+++ template/src/App.js
@@ -6,11 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
+        <p>Edit <code>src/App.js</code>, save, then commit changes to see if husky triggers pre-commit githook.</p>
+        <a className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
